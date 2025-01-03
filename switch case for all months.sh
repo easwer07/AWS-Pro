333b056b4061704d35e15f4e0315034402903d8f
easwer <dharavatheaswer@gmail.com>
2025-01-03 10:02:32 +0530

    correction

--- switch case for all months.sh
+++ switch case for all months.sh
@@ -3,9 +3,9 @@ shopt -s nocasematch
 echo "Enter name of month"
 read month
 case $month in
-february
+february)
 echo "28/29 day in a $month";;
- April | June | September | November)
+April | June | September | November)
 echo "30 days in $month";;
 January | March | May | July | August | October | December)
 echo "31 days in $month";;
