d534b678c0e88f706be2b990bf32b3ef6be13713
easwer <dharavatheaswer@gmail.com>
2025-01-03 14:39:27 +0530

    1st update in numbers script

---
+++ numbers script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ # Initializing the variable
+ a=20
+ if [ $a < 10 ] 
+then  
+      # If variable less than 10    
+      echo "a is less than 10" 
+elif [ $a < 25 ] 
+then  
+      # If variable less than 25  
+      echo "a is less than 25" 
+else   
+     # If variable is greater than 25   
+     echo "a is greater than 25"  
+fi 
